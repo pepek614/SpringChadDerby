f9a893ba2e0017b823bfb5bbafb8facd394c9c65
pepek614 <pepekr614@gmail.com>
2022-07-05 10:33:20 +0200

    update

--- spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -1,6 +1,12 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
+	private FortuneService fortuneService;
+	
+	public TrackCoach(FortuneService fortuneService) {
+		super();
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -11,7 +17,7 @@ public class TrackCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Just do it! " + fortuneService.getFortune();
 	}
 
 }
