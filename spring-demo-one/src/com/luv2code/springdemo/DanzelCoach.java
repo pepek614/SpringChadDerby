5e46280e58f154ffe2a304b1e2f293b2b4bc0da1
pepek614 <pepekr614@gmail.com>
2022-07-05 08:13:17 +0200

    add everything

---
+++ spring-demo-one/src/com/luv2code/springdemo/DanzelCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+public class DanzelCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "You got to pump it up";
+	}
+	
+
+}
