c05617b0670cd17acf33b28dbdf084e7d2fb4f7f
pepek614 <pepekr614@gmail.com>
2022-07-06 20:37:52 +0200

    1. point of developer proces (create setters methods in class of injection)

--- spring-demo-one/src/com/luv2code/springdemo/SetterDemoClass.java
+++ spring-demo-one/src/com/luv2code/springdemo/SetterDemoClass.java
@@ -12,7 +12,8 @@ public class SetterDemoClass {
 		//retrieve bean from spring container
 		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		// call methods on the bean
-		
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
 		//close the context
 		context.close();
 	}
