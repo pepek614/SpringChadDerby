68e98846ef230cfadada8c6d5f92574645fbc5d5
pepek614 <pepekr614@gmail.com>
2022-07-19 19:44:14 +0200

    after section 12

--- spring-demo-annotations/src/com/luv2code/springdemo/aaDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/aaDemoApp.java
@@ -1,15 +1,47 @@
 package com.luv2code.springdemo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class aaDemoApp {
 
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = 
+		/*AnnotationConfigApplicationContext context = 
 				new AnnotationConfigApplicationContext(aaConfigFile.class); 
 		Coach theCoach = context.getBean("baseballCoach", Coach.class);
 		System.out.println(theCoach.getDailyFortune());
 		System.out.println(theCoach.getDailyWorkout());
+	*/
+		int k = 3;
+		int n = 6;
+		ArrayList<Integer> arr = new ArrayList<>();
+		arr.add(5);
+		arr.add(6);
+		arr.add(8);
+		arr.add(10);
+		arr.add(12);
+		reverseInGroups(arr,n,k);
+	}
+		public static void reverseInGroups(ArrayList<Integer> arr, int n, int k) {
+			int[] first = new int[k];
+	        for (int i =0; i<k;i++){
+	            first[i]=arr.get(i);
+	        }
+	        int[] second = new int[n-k];
+	        int a =0;
+	        for (int i = k; i<arr.size();i++){
+	            second[a] = arr.get(i);
+	                    a++;
+	        }
+	        for (int b : first){
+	            System.out.print(b + " ");
+	        }
+	        for (int c: second){
+	            System.out.print(c + " ");
+	        }
+	    }
 	}
 
-}
+
---
+++ spring-mvc-demo/WebContent/WEB-INF/view/helloworld.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+Hello World of Spring!<a href="http://www.wykop.pl>" portal></a>
+<br><br>
+
+Student name: ${param.studentName}
+
+<br><br>
+
+The message : ${message}
+</body>
+
+</html>
